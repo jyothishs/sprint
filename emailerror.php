5926eef22c1aeeb24fde1255d57331fb09e7328b
jyothish <jyothish>
2016-09-05 10:59:03 +0530

    done

---
+++ emailerror.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+	<?php $this->load->helper('url'); ?>
+
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url().'css/bootstrap.min.css'; ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url().'css/style.css'; ?>">
+	<script type="text/javascript" src="?<?php echo base_url().'js/bootsutrap.min.js';?>"></script>
+	<script type="text/javascript" src="<?php echo base_url().'js/jquery-2.2.3.min.js';?>"></script>
+	<title></title>
+</head>
+<body>
+    <div class="container-fluid">
+		<div class="row head white">
+			<div class="col-md-offset-1 col-md-2">
+				<img src="<?php echo base_url().'images/fb_head.png';  ?>">	
+			</div>
+			<div class="col-md-1">
+				<input type="submit" name="" value="sign up" class="sign_up btn btn-success"></div>
+			</div>
+			<div class="row color" >
+				<div class="col-md-offset-3 col-md-6 email_error ">
+				<label class="facebook_login"><b>Facebook login</b></label>
+				<hr>
+			    <div class="col-md-offset-1 col-md-3 pad_5   ">
+			       <label> Email or Phone</label>
+			    </div>
+			    <div class="col-md-5 pad_5">
+			       <input type="text" name="" class="form-control">
+			    </div>
+			    <div class="col-md-offset-1 col-md-3 pad_5">
+			       <label> Password</label>
+			    </div>
+			    <div class="col-md-5 pad_5">
+			       <input type="text" name="" class="form-control">
+			    </div>
+			    <div class=" col-md-offset-4 col-md-5 pad_5 ">
+			    	<input type="checkbox" name=""><label>&nbsp;Keep me logged in </label>
+			    </div>
+			    <div class=" col-md-offset-4 col-md-5 pad_5 ">
+                      <input type="submit" name=""  value="log in" class="btn btn-success">
+                      or <a href="">Sign up for Facebook</a>
+                </div>
+                <div class="col-md-offset-4 col-md-5 gen_pad">
+                	<a href="">Forgotten password?</a>
+               		 </div>
+				</div>
+		</div>
+</div>
+ <div class="row">
+                 <div class="col-md-offset-2 col-md-8 ">
+
+		    	     <ul class="list-inline pad_6">
+		    	     	<li><a href=""> English(UK)</a></li>
+		    	     	<li><a href="">Malayalam</a></li>
+		    	     	<li><a href="">Hindi</a></li>
+		    	     	<li><a href="">Tamil</a></li>
+		    	     	<li><a href="">Kannada</a></li>
+		    	     	<li><a href="">Portugees</a></li>
+		    	     	<li><a href="">france</a></li>
+		    	     </ul>
+		    	     	
+		    	     	<hr>
+	            </div>
+		    	     
+		    	 <div class="col-md-offset-2 col-md-8">
+		    	 <ul class="list-inline">
+		    	 	<li>
+		    	 	<a href="">Sign Up</a>
+		    	 	</li>
+		    	 	<li>
+		    	 		<a href="">Log In</a>
+		    	 	</li>
+		    	    <li>
+		    	 		<a href="">Messenger</a>
+		    	 	</li>
+		    	 	<li>
+		    	 		<a href="">Facebook Lite</a>
+		    	 	</li>
+		    	 	<li>
+		    	 		<a href="">mobile</a>
+		    	 	</li>
+		    	 	<li>
+		    	 		<a href="">Find Friends</a>
+		    	 	</li>
+		    	 	<li>
+		    	 		<a href="">Badges</a>
+		    	 	</li>
+		    	 	<li>
+		    	 		<a href="">People</a>
+		    	 	</li>
+		    	 	<li>
+		    	 		<a href="">Pages</a>
+		    	 	</li>
+		    	 	<li>
+		    	 		<a href="">Places</a>
+		    	 	</li>
+		    	 	<li>
+		    	 		<a href="">Games</a>
+		    	 	</li>
+		    	 	
+		    	 		<a href="">Locations</a>
+		    	 	</li>
+		    	 </ul>
+		    	 <ul class="list-inline">
+		    	 
+		    	 	<li>
+		    	 		<a href="">Celebreties</a>
+		    	 	</li>
+		    	 	<li>
+		    	 		<a href="">Messenger</a>
+		    	 	</li>
+		    	 	<li>
+		    	 		<a href="">Groups</a>
+		    	 	</li>
+		    	 	<li>
+		    	 		<a href="">Moments</a>
+		    	 	</li>
+		    	 	<li>
+		    	 		<a href="">about</a>
+		    	 	</li>
+		    	 	<li>
+		    	 		<a href="">Create advert</a>
+		    	 	</li>
+		    	 	<li>
+		    	 		<a href="">About page</a>
+		    	 	</li>
+		    	 	<li>
+		    	 		<a href="">Devolopers</a>
+		    	 	</li>
+		    	 	<li>
+		    	 		<a href="">Careers</a>
+		    	 	</li>
+		    	 	<li>
+		    	 		<a href="">Privacy</a>
+		    	 	</li>
+		    	 	<li>
+		    	 		<a href="">Cookies</a>
+		    	 	</li>
+		    	 	</ul>
+		    	 	<ul class="list-inline">
+		    	 		<li>
+		    	 		<a href="">AdChoices</a>
+		    	 	</li>
+		    	 	<li>
+		    	 		<a href="">Terms</a>
+		    	 	</li>
+		    	 	<li>
+		    	 		<a href="">Helps</a>
+		    	 	</li>
+		    	 	</ul>
+		    	 	
+		    	    </div>
+		    	    <div class="col-md-offset-2 col-md-8 pad_6">Facebook@2016
+		    	    	
+		    	    </div>
+		    	    </div>
+
+</body>
+</html>
