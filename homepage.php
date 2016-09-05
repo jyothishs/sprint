676174017c0711f25ffb0c214ae028aa8d4abf8a
jyothish <jyothish>
2016-09-05 10:53:07 +0530

    done

---
+++ homepage.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+	<?php $this->load->helper('url'); ?>
+
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url().'css/bootstrap.min.css'; ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url().'css/style.css'; ?>">
+	<script type="text/javascript" src="<?php echo base_url().'js/bootsutrap.min.js';?>"></script>
+	<script type="text/javascript" src="<?php echo base_url().'js/jquery-2.2.3.min.js';?>"></script>
+	<title></title>
+</head>
+<body>
+   <div class=" container-fluid">
+   		<div class="row head">
+   		   	<div class=" col-md-5 search_pad">
+   		   		<input type="text" name="" class="form-control " placeholder="Search Facebook">
+   		   	</div>
+   		   	<div class="col-md-offset-1 col-md-3">
+   		   	    <img src="<?php echo base_url().'images/haris.png';?>" class="search_photo">
+   		   	    <label style="color:white">Haris</label>
+   		    </div>
+   		</div>    
+        <div class="row color">
+        <div class="col-md-3">
+          	<div class="col-md-12 pad_5">
+               <img src="<?php echo base_url().'images/haris.png';?>" class="tags">
+               <label>Haris np</label>
+            </div>
+           	<div class="col-md-12 pad_5">
+               <img src="<?php echo base_url().'images/edit.png';?>" class="tags">
+               <label>Edit profile</label>   
+            </div>
+        
+        </div>	
+          <div class="col-md-6 cnt_color pad_5">
+        	<div class="col-md-4 cnt_color pad_5">
+                <img src="<?php echo base_url().'images/edit.png';?>" class="tag1">
+                <label>Update status</label>
+             </div>	
+              <div class="col-md-4 cnt_color pad_5">
+                      <img src="<?php echo base_url().'images/edit.png';?>" class="tag1">
+                      <label>Add Photos/Video</label>
+              </div>
+              <div class="col-md-4 cnt_color pad_5">
+                      <img src="<?php echo base_url().'images/edit.png';?>" class="tag1">
+                      <label>Write Note</label>
+               
+              </div>
+                <div class="col-md-12 tag3">
+              		  <hr>
+                <div class=" col-md-6 status_color pad_5 ">
+                  <img src="<?php echo base_url().'images/haris.png';?>" class="tag2">
+   		   	       What's on your mind? 
+              	
+                 </div>
+                
+                 	
+                 </div>
+             </div>
+             <div class="col-md-2 pad_select">
+             <select class="form-control "><option>Your Ads</option></select>
+             	
+             </div>
+        
+         <div class="col-md-12"></div>
+            <div class=" col-md-offset-3 col-md-6 cnt_color margin">
+      
+             </div>
+             	
+             </div>
+   		    	
+   	
+   			
+ </div>
+ </div>
+   	
+  
+
+</body>
+</html>
